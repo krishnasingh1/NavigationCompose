aa14ef67797fe1c56bc1a7c24e96551b1829788a
Krishana Pal <krishnasingh.ks62@gmail.com>
2023-02-02 23:23:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationcompose/HomeScreen.kt
@@ -0,0 +1,44 @@
+package com.example.navigationcompose
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier.clickable { navController.navigate(route = Screen.Detail.route) },
+            text = "Home",
+            color = MaterialTheme.colors.primary,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(navController = rememberNavController())
+
+}
---
+++ app/src/main/java/com/example/navigationcompose/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.navigationcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.navigationcompose.ui.theme.NavigationComposeTheme
+
+class MainActivity : ComponentActivity() {
+    lateinit var navController: NavHostController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NavigationComposeTheme {
+                // A surface container using the 'background' color from the theme
+                navController = rememberNavController()
+                SetupNavGraph(navController = navController)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    NavigationComposeTheme {
+        Greeting("Android")
+    }
+}
